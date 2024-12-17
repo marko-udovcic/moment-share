56113f49ec6a7fe59e688eb8f8bf0a35d9b01351
marko udovcic <markoudovcic53@gmail.com>
2024-12-17 20:43:10 +0100

    Add configuration file

---
+++ social-app/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      // Preusmerava zahteve sa '/followme' na vaš backend
+      "/followme": {
+        target: "http://localhost:5000", // Backend server
+        changeOrigin: true, // Menja origin ako je potrebno
+        secure: false, // Ako vaš backend koristi HTTPS, postavite na true
+      },
+      // Preusmerava zahteve sa '/following' na vaš backend
+      "/following": {
+        target: "http://localhost:5000", // Backend server
+        changeOrigin: true, // Menja origin ako je potrebno
+        secure: false, // Ako vaš backend koristi HTTPS, postavite na true
+      },
+    },
+  },
+});
