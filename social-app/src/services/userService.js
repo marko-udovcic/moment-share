3ac130b53bb5dce23cefa8b88baabbd000ec1c1c
marko udovcic <markoudovcic53@gmail.com>
2024-12-26 16:40:13 +0100

    feat: add userService file for manage crud operation

---
+++ social-app/src/services/userService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const appApi = axios.create({
+  baseURL: "http://localhost:5000",
+});
+export async function getAllUsers() {
+  const response = await appApi.get("/allusers");
+  return response.data;
+}
+
+export async function addUser(user) {
+  return await appApi.post("/allusers", user);
+}
+
+export async function updateUser(user) {
+  return await appApi.patch(`/allusers/${user.id}`, user);
+}
+
+export async function deleteUser(id) {
+  return await appApi.delete(`/allusers/${id}`);
+}
