695960b6219372cb5b5170b9a9619477dcad625f
marko udovcic <markoudovcic53@gmail.com>
2024-12-26 16:41:11 +0100

    feat: add arrayUtils file with shuffleUsers algorithm

---
+++ social-app/src/utils/arrayUtils.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+export function shuffleUsers(list) {
+  let shuffledList = [...list];
+  for (let i = shuffledList.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledList[i], shuffledList[j]] = [shuffledList[j], shuffledList[i]];
+  }
+  return shuffledList;
+}
