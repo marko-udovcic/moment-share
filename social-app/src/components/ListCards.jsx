5afde5e39af2ffe425dbaa3a84cf5ce964e3ab93
marko udovcic <markoudovcic53@gmail.com>
2024-12-28 16:45:14 +0100

    refactor: change deleteMoment function

--- social-app/src/components/ListCards.jsx
+++ social-app/src/components/ListCards.jsx
@@ -1,8 +1,13 @@
 import { useContext } from "react";
 import { myContext } from "../context/Context";
 import Card from "./Card";
+import Button from "./Button";
+import PropTypes from "prop-types";
 
-export default function ListCards() {
+export default function ListCards({ setListMoments }) {
+  const { listMoments, fetchData } = useContext(myContext);
+  const filteredListMoments = listMoments.filter((moment) => moment.username === "m.udovcic");
+
   function getLuminance(color) {
     const hex = color.replace("#", "");
     const r = parseInt(hex.slice(0, 2), 16);
@@ -12,26 +17,58 @@ export default function ListCards() {
     const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
     return luminance;
   }
-
-  const { listMoments } = useContext(myContext);
-
-  const filteredListMoments = listMoments.filter((moment) => moment.username === "m.udovcic");
+  async function deleteMoment(id, url) {
+    const res = await fetch(`${url}/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!res.ok) {
+      throw new Error("Failed to delete user");
+    } else {
+      fetchData("/posts", setListMoments);
+      console.log("Successfully deleted");
+    }
+  }
+  function handleRemoveMoment(id) {
+    const confirm = window.confirm("are you sure you want remove user");
+    if (!confirm) return;
+    deleteMoment(id, "/posts");
+  }
 
   return (
-    <div className="z-0 m-auto mx-3 grid min-h-[50vh] grid-cols-1 gap-10 rounded-lg bg-gray-800 sm:grid-cols-2 md:mx-10 md:grid-cols-3 lg:grid-cols-4 lg:p-5">
+    <div
+      className="z-0 m-auto mx-3 grid min-h-[50vh] grid-cols-1 gap-10 rounded-lg bg-gray-800 sm:grid-cols-2 md:mx-10
+        md:grid-cols-3 lg:grid-cols-4 lg:p-5"
+    >
       {filteredListMoments && filteredListMoments.length > 0 ? (
         filteredListMoments.map((moment) => {
-          const luminance = getLuminance(moment.color);
-          const textColor = luminance > 128 ? "text-black" : "text-white";
+          const textColor = getLuminance(moment.color) > 128 ? "text-black" : "text-white";
+          const date = moment.created_at.split(" ")[0];
 
           return (
             <Card
               key={moment.id}
-              className={`relative h-[20rem] content-center rounded-md text-center ${textColor}`}
-              style={{ backgroundColor: moment.color }}
+              className={`relative flex h-[20rem] flex-col justify-between overflow-hidden rounded-[20px] p-3 text-center ${textColor}
+                bg-white`}
             >
-              <h3>{moment.moment}</h3>
-              <p className="absolute bottom-0 p-1">{moment.created_at}</p>
+              <div
+                className="relative flex flex-grow items-center justify-center overflow-auto rounded-[20px]"
+                style={{ backgroundColor: moment.color }}
+              >
+                <div className="max-h-full overflow-y-auto text-center">
+                  <h3 className="break-words text-lg font-medium">{moment.moment}</h3>
+                </div>
+              </div>
+              <p className="absolute bottom-0 left-0 m-2 text-sm text-black">{date}</p>
+              <Button
+                onClick={() => handleRemoveMoment(moment.id)}
+                className="mt-3 self-end rounded-lg p-1 hover:bg-red-600 hover:text-white transition duration-300 text-red-600
+                  font-semibold"
+              >
+                Delete moment
+              </Button>
             </Card>
           );
         })
@@ -41,3 +78,6 @@ export default function ListCards() {
     </div>
   );
 }
+ListCards.propTypes = {
+  setListMoments: PropTypes.func,
+};
