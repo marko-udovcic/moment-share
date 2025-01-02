f92757e9be803cd97b05a92dd934929982eab9cc
marko udovcic <markoudovcic53@gmail.com>
2025-01-02 21:02:30 +0100

    add query client for using react-form

--- social-app/src/App.jsx
+++ social-app/src/App.jsx
@@ -5,33 +5,36 @@ import Home from "./pages/Home";
 import Nav from "./components/Nav";
 import { useState } from "react";
 import Explore from "./pages/Explore";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App() {
+  const queryClient = new QueryClient();
   const [showFollowers, setShowFollowers] = useState(false);
   const [showFollowing, setShowFollowing] = useState(false);
 
   return (
-    <Router>
-      <div className="sm:max-w-[calc(100%-160px) mx-auto min-h-screen border-[2px] border-t-0 border-white bg-slate-900 px-0 pt-4">
-        <Nav />
-        <Routes>
-          <Route path="/explore" element={<Explore />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/profile"
-            element={
-              <Profile
-                showFollowers={showFollowers}
-                showFollowing={showFollowing}
-                setShowFollowers={setShowFollowers}
-                setShowFollowing={setShowFollowing}
-              />
-            }
-          />
-          {/* Make sure Home is your profile page */}
-        </Routes>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div className="sm:max-w-[calc(100%-160px) mx-auto min-h-screen border-[2px] border-t-0 border-white bg-slate-900 px-0 pt-4">
+          <Nav />
+          <Routes>
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/profile"
+              element={
+                <Profile
+                  showFollowers={showFollowers}
+                  showFollowing={showFollowing}
+                  setShowFollowers={setShowFollowers}
+                  setShowFollowing={setShowFollowing}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
