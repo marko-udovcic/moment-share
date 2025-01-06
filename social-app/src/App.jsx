b8c1ddd604328ccd3ff56c11dd3b376f76e64151
marko udovcic <markoudovcic53@gmail.com>
2025-01-06 18:28:00 +0100

    add context provider

--- social-app/src/App.jsx
+++ social-app/src/App.jsx
@@ -6,6 +6,7 @@ import Nav from "./components/Nav";
 import { useState } from "react";
 import Explore from "./pages/Explore";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ProfileProvider } from "./context/ProfileContext";
 
 export default function App() {
   const queryClient = new QueryClient();
@@ -13,28 +14,30 @@ export default function App() {
   const [showFollowing, setShowFollowing] = useState(false);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <div className="sm:max-w-[calc(100%-160px) mx-auto min-h-screen border-[2px] border-t-0 border-white bg-slate-900 px-0 pt-4">
-          <Nav />
-          <Routes>
-            <Route path="/explore" element={<Explore />} />
-            <Route path="/create" element={<Create />} />
-            <Route path="/" element={<Home />} />
-            <Route
-              path="/profile"
-              element={
-                <Profile
-                  showFollowers={showFollowers}
-                  showFollowing={showFollowing}
-                  setShowFollowers={setShowFollowers}
-                  setShowFollowing={setShowFollowing}
-                />
-              }
-            />
-          </Routes>
-        </div>
-      </Router>
-    </QueryClientProvider>
+    <ProfileProvider>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <div className="sm:max-w-[calc(100%-160px) mx-auto min-h-screen border-[2px] border-t-0 border-white bg-slate-900 px-0 pt-4">
+            <Nav />
+            <Routes>
+              <Route path="/explore" element={<Explore />} />
+              <Route path="/create" element={<Create />} />
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/profile"
+                element={
+                  <Profile
+                    showFollowers={showFollowers}
+                    showFollowing={showFollowing}
+                    setShowFollowers={setShowFollowers}
+                    setShowFollowing={setShowFollowing}
+                  />
+                }
+              />
+            </Routes>
+          </div>
+        </Router>
+      </QueryClientProvider>
+    </ProfileProvider>
   );
 }
