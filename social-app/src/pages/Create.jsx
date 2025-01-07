a56710c4a0683712c540801d751313f4427e1bdc
marko udovcic <markoudovcic53@gmail.com>
2025-01-07 11:32:24 +0100

    refactor create component for better readability(add logic to hooks folder)

--- social-app/src/pages/Create.jsx
+++ social-app/src/pages/Create.jsx
@@ -1,23 +1,17 @@
 import { useRef, useEffect } from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useProfile } from "../context/ProfileContext";
+import { useAddPost } from "../features/create/hooks/useAddPost";
+import Button from "../components/ui/Button";
 
 export default function Create() {
+  const { refreshData } = useProfile();
   const [moment, setMoment] = useState("");
   const [color, setColor] = useState("#f1f5a2");
   const inputEl = useRef(null);
-
+  const addPost = useAddPost();
   const navigator = useNavigate();
-  async function handleAddPost(post) {
-    await fetch("http://localhost:5000/posts", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(post),
-    });
-    return;
-  }
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -31,14 +25,14 @@ export default function Create() {
       username,
       created_at: new Date().toLocaleString("en-GB"),
     };
-    handleAddPost(newPost);
-    setMoment("");
-    navigator("/profile");
+    addPost(newPost, {
+      onSuccess: () => {
+        setMoment("");
+        navigator("/profile");
+        refreshData();
+      },
+    });
   }
-
-  const handleChange = (e) => {
-    setColor(e.target.value);
-  };
 
   useEffect(() => {
     function callback(e) {
@@ -55,26 +49,23 @@ export default function Create() {
 
   return (
     <div className="flex min-h-80 w-full items-center justify-center text-white">
-      <form
-        className="mx-auto mt-9 flex w-full flex-col rounded-xl bg-blue-400 p-7 md:w-1/2"
-        onSubmit={handleSubmit}
-      >
+      <form className="mx-auto mt-9 flex w-full flex-col rounded-xl p-7 md:w-1/2" onSubmit={handleSubmit}>
         <label className="p-3 font-sans text-xl">Choose color of card: </label>
-        <input className="mx-3" type="color" value={color} onChange={handleChange} />
+        <input className="mx-3" type="color" value={color} onChange={(e) => setColor(e.target.value)} />
         <label className="p-3 font-sans text-xl">Enter your moment </label>
         <textarea
-          className="mb-5 h-[200px] rounded-xl p-5 text-black"
+          className="mb-5 h-[200px] rounded-xl p-5 bg-blue-950 text-white"
           value={moment}
           ref={inputEl}
           onChange={(e) => setMoment(e.target.value)}
           placeholder="Share your current moment..."
         ></textarea>
-        <button
-          className="rounded-xl border border-transparent bg-blue-950 p-2 font-sans font-semibold duration-700 hover:border-white
-            hover:bg-blue-400 md:mx-auto md:w-1/2 focus:border-blue-500"
+        <Button
+          className="rounded-xl border border-transparent p-2 font-sans font-semibold duration-700 hover:border-white bg-blue-400
+            md:mx-auto md:w-1/2 focus:border-blue-500"
         >
           Add moment
-        </button>
+        </Button>
       </form>
     </div>
   );
