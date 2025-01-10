781ffce2e6aea1332f6619ed9b17f630ae57072d
marko udovcic <markoudovcic53@gmail.com>
2025-01-10 15:39:09 +0100

    update ui

--- social-app/src/pages/Create.jsx
+++ social-app/src/pages/Create.jsx
@@ -52,20 +52,22 @@ export default function Create() {
       <form className="mx-auto mt-9 flex w-full flex-col rounded-xl p-7 md:w-1/2" onSubmit={handleSubmit}>
         <label className="p-3 font-sans text-xl">Choose color of card: </label>
         <input className="mx-3" type="color" value={color} onChange={(e) => setColor(e.target.value)} />
-        <label className="p-3 font-sans text-xl">Enter your moment </label>
-        <textarea
-          className="mb-5 h-[200px] rounded-xl p-5 bg-blue-950 text-white"
-          value={moment}
-          ref={inputEl}
-          onChange={(e) => setMoment(e.target.value)}
-          placeholder="Share your current moment..."
-        ></textarea>
-        <Button
-          className="rounded-xl border border-transparent p-2 font-sans font-semibold duration-700 hover:border-white bg-blue-400
-            md:mx-auto md:w-1/2 focus:border-blue-500"
-        >
-          Add moment
-        </Button>
+        <div className="flex flex-col bg-blue-950 rounded-sm relative mt-4">
+          <textarea
+            className="mb-5 h-[15rem] rounded-xl p-7 bg-blue-950 text-white"
+            value={moment}
+            ref={inputEl}
+            onChange={(e) => setMoment(e.target.value)}
+            placeholder="Share your current moment..."
+          ></textarea>
+          <Button
+            className="w-1/2 mx-auto mb-5 flex items-center justify-center rounded-xl border border-transparent p-2 font-sans
+              font-semibold duration-700 hover:border-white bg-blue-400 md:mx-auto md:w-1/2 focus:border-blue-500
+              lg:absolute lg:right-5 lg:bottom-5 lg:w-[200px]"
+          >
+            Add moment
+          </Button>
+        </div>
       </form>
     </div>
   );
