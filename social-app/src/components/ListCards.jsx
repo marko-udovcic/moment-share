e6c6817c63b14d37f2fbf310755fe9ad6b24d571
marko udovcic <markoudovcic53@gmail.com>
2025-01-14 17:01:37 +0100

    refactor: remove function from component

--- social-app/src/components/ListCards.jsx
+++ social-app/src/components/ListCards.jsx
@@ -2,20 +2,12 @@ import { RxDotsVertical } from "react-icons/rx";
 import Card from "./ui/Card";
 import Button from "./ui/Button";
 import { useProfile } from "../context/ProfileContext";
+import { getLuminance } from "../utils/colorUtils";
 
 export default function ListCards() {
   const { listMoments, fetchData, setListMoments } = useProfile();
   const filteredListMoments = listMoments.filter((moment) => moment.username === "m.udovcic");
 
-  function getLuminance(color) {
-    const hex = color.replace("#", "");
-    const r = parseInt(hex.slice(0, 2), 16);
-    const g = parseInt(hex.slice(2, 4), 16);
-    const b = parseInt(hex.slice(4, 6), 16);
-
-    const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-    return luminance;
-  }
   async function deleteMoment(id, url) {
     const res = await fetch(`${url}/${id}`, {
       method: "DELETE",
