f6ee2033a7a04a28d180cb4811a45199b881ab3a
marko udovcic <markoudovcic53@gmail.com>
2025-01-16 22:14:31 +0100

    refactor: update ui and add Nav component

--- social-app/src/pages/Create.jsx
+++ social-app/src/pages/Create.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useProfile } from "../context/ProfileContext";
 import { useAddPost } from "../features/create/hooks/useAddPost";
 import Button from "../components/ui/Button";
+import Nav from "../components/Nav";
 
 export default function Create() {
   const { refreshData } = useProfile();
@@ -48,27 +49,30 @@ export default function Create() {
   }, []);
 
   return (
-    <div className="flex min-h-80 w-full items-center justify-center text-white">
-      <form className="mx-auto mt-9 flex w-full flex-col rounded-xl p-7 md:w-1/2" onSubmit={handleSubmit}>
-        <label className="p-3 font-sans text-xl">Choose color of card: </label>
-        <input className="mx-3" type="color" value={color} onChange={(e) => setColor(e.target.value)} />
-        <div className="flex flex-col bg-blue-950 rounded-sm relative mt-4">
-          <textarea
-            className="mb-5 h-[15rem] rounded-xl p-7 bg-blue-950 text-white"
-            value={moment}
-            ref={inputEl}
-            onChange={(e) => setMoment(e.target.value)}
-            placeholder="Share your current moment..."
-          ></textarea>
-          <Button
-            className="w-1/2 mx-auto mb-5 flex items-center justify-center rounded-xl border border-transparent p-2 font-sans
-              font-semibold duration-700 hover:border-white bg-blue-400 md:mx-auto md:w-1/2 focus:border-blue-500
-              lg:absolute lg:right-5 lg:bottom-5 lg:w-[200px]"
-          >
-            Add moment
-          </Button>
-        </div>
-      </form>
-    </div>
+    <>
+      <Nav />
+      <div className="flex min-h-80 w-full items-center justify-center text-white">
+        <form className="mx-auto mt-9 flex w-full flex-col rounded-xl p-7 md:w-1/2" onSubmit={handleSubmit}>
+          <label className="p-3 font-sans text-xl">Choose color of card: </label>
+          <input className="mx-3" type="color" value={color} onChange={(e) => setColor(e.target.value)} />
+          <div className="flex flex-col bg-blue-950 rounded-sm relative mt-4">
+            <textarea
+              className="mb-5 h-[15rem] rounded-xl p-7 bg-blue-950 text-white"
+              value={moment}
+              ref={inputEl}
+              onChange={(e) => setMoment(e.target.value)}
+              placeholder="Share your current moment..."
+            ></textarea>
+            <Button
+              className="w-1/2 mx-auto mb-5 flex items-center justify-center rounded-xl border border-transparent p-2 font-sans
+                font-semibold duration-700 hover:border-white bg-blue-400 md:mx-auto md:w-1/2 focus:border-blue-500
+                lg:absolute lg:right-5 lg:bottom-5 lg:w-[200px]"
+            >
+              Add moment
+            </Button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
