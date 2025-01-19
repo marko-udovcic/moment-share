e0a110d9137bd940f6cd336e94f6b0365f3745a4
marko udovcic <markoudovcic53@gmail.com>
2025-01-19 19:43:21 +0100

    auth: add logout function

--- social-app/src/features/auth/services/apiAuth.js
+++ social-app/src/features/auth/services/apiAuth.js
@@ -77,3 +77,7 @@ export async function getCurrentUser() {
     throw err;
   }
 }
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
+}
