7f5f5fd301c64786675686a602b198c3f5a47889
marko udovcic <markoudovcic53@gmail.com>
2025-01-27 15:28:23 +0100

    refactor: update hook(add id to setUser)

--- social-app/src/features/profile/hooks/useCurrentUser.js
+++ social-app/src/features/profile/hooks/useCurrentUser.js
@@ -1,20 +1,30 @@
 import { useEffect, useState } from "react";
 import { getCurrentUser } from "../../auth/services/apiAuth";
+
 export function useCurrentUser() {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null); // Stores the user data
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    async function displayUserInfo() {
+    async function fetchUserData() {
       try {
         const fetchedUser = await getCurrentUser();
         if (fetchedUser) {
-          setUser({ username: fetchedUser.user_metadata.username, email: fetchedUser.email });
+          setUser({
+            username: fetchedUser.user_metadata.username,
+            email: fetchedUser.email,
+            id: fetchedUser.id,
+          });
         }
       } catch (err) {
-        console.error("Erorr during loading user:", err.message);
+        console.error("Error during loading user:", err.message);
+      } finally {
+        setLoading(false);
       }
     }
 
-    displayUserInfo();
+    fetchUserData();
   }, []);
-  return user;
+
+  return { user, loading };
 }
