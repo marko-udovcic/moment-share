1d8f2876d32ffb08afd65a427916f3b540df26ac
marko udovcic <markoudovcic53@gmail.com>
2025-01-27 15:22:46 +0100

    refactor: change newPost object for storing in supabase db

--- social-app/src/pages/Create.jsx
+++ social-app/src/pages/Create.jsx
@@ -5,11 +5,12 @@ import { useProfile } from "../context/ProfileContext";
 import { useAddPost } from "../features/create/hooks/useAddPost";
 import Button from "../components/ui/Button";
 import Nav from "../components/Nav";
-
+import { useCurrentUser } from "../features/profile/hooks/useCurrentUser";
 export default function Create() {
   const { refreshData } = useProfile();
   const [moment, setMoment] = useState("");
   const [color, setColor] = useState("#1F2937");
+  const { user: currentUser } = useCurrentUser();
   const inputEl = useRef(null);
   const addPost = useAddPost();
   const navigator = useNavigate();
@@ -17,15 +18,13 @@ export default function Create() {
   function handleSubmit(e) {
     e.preventDefault();
     if (!moment) return;
-    const id = crypto.randomUUID();
-    const username = "m.udovcic";
+
     const newPost = {
-      id,
-      moment,
+      content: moment,
+      user_id: currentUser.id,
       color,
-      username,
-      created_at: new Date().toLocaleString("en-GB"),
     };
+
     addPost(newPost, {
       onSuccess: () => {
         setMoment("");
@@ -55,9 +54,9 @@ export default function Create() {
         <form className="mx-auto mt-9 flex w-full flex-col rounded-xl p-7 md:w-1/2" onSubmit={handleSubmit}>
           <label className="p-3 font-sans text-xl">Choose color of card: </label>
           <input className="mx-3" type="color" value={color} onChange={(e) => setColor(e.target.value)} />
-          <div className="flex flex-col bg-blue-950 rounded-sm relative mt-4">
+          <div className="flex flex-col bg-slate-800 rounded-sm relative mt-4">
             <textarea
-              className="mb-5 h-[15rem] rounded-xl p-7 bg-blue-950 text-white"
+              className="mb-5 h-[15rem] rounded-xl p-7 bg-slate-800 text-white focus:outline-none"
               value={moment}
               ref={inputEl}
               onChange={(e) => setMoment(e.target.value)}
@@ -65,8 +64,8 @@ export default function Create() {
             ></textarea>
             <Button
               className="w-1/2 mx-auto mb-5 flex items-center justify-center rounded-xl border border-transparent p-2 font-sans
-                font-semibold duration-700 hover:border-white bg-blue-400 md:mx-auto md:w-1/2 focus:border-blue-500
-                lg:absolute lg:right-5 lg:bottom-5 lg:w-[200px]"
+                font-semibold duration-700 hover:border-white bg-blue-400 md:mx-auto md:w-1/2 focus:outline-none lg:absolute
+                lg:right-5 lg:bottom-5 lg:w-[200px]"
             >
               Add moment
             </Button>
