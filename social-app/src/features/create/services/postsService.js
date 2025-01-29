6064f05102d9ea22802dbeb72aeb0cd1e167deb6
marko udovcic <markoudovcic53@gmail.com>
2025-01-29 20:30:28 +0100

    feat: add post logic with supabase client

--- social-app/src/features/create/services/postsService.js
+++ social-app/src/features/create/services/postsService.js
@@ -1,9 +1,16 @@
-import axios from "axios";
+// import axios from "axios";
 
-const appApi = axios.create({
-  baseURL: "http://localhost:5000",
-});
+// const appApi = axios.create({
+//   baseURL: "http://localhost:5000",
+// });
+
+// export async function addPost(post) {
+//   return await appApi.post("/posts", post);
+// }
+import supabase from "../../../services/supaBaseClient";
 
 export async function addPost(post) {
-  return await appApi.post("/posts", post);
+  const { data, error } = await supabase.from("posts").insert([post]).select();
+  if (error) throw new Error(error.message);
+  return data;
 }
