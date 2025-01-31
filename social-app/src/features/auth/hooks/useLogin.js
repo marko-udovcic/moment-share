48393423c5955d42a0f96418a0999b09d881fe83
marko udovcic <markoudovcic53@gmail.com>
2025-01-31 10:54:11 +0100

    refactor: setCurrentUser from zustand with current login user data

--- social-app/src/features/auth/hooks/useLogin.js
+++ social-app/src/features/auth/hooks/useLogin.js
@@ -1,17 +1,34 @@
 import { useMutation } from "@tanstack/react-query";
 import { login as loginApi } from "../services/apiAuth";
 import { useNavigate } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
+import { useProfileStore } from "../../../context/zustand/useProfileStore"; // importuj Zustand store
+
 export function useLogin() {
+  const query = useQueryClient();
   const navigate = useNavigate();
+  const setCurrentUser = useProfileStore((state) => state.setCurrentUser);
+
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      console.log("ispis useraa", user);
-      navigate("/profile");
+      const currentUser = user.user
+        ? {
+            username: user.user.user_metadata?.username || "No username",
+            email: user.user.email,
+            id: user.user.id,
+          }
+        : null;
+      query.setQueryData(["currentUser"], currentUser);
+
+      setCurrentUser(currentUser);
+
+      navigate("/profile", { replace: true });
     },
     onError: (err) => {
-      console.log("Error from login is", err);
+      console.log("Error from login:", err);
     },
   });
+
   return { login, isLoading };
 }
