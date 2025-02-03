0722e5b698d8386182db4a19ba01f3fd012039d2
marko udovcic <markoudovcic53@gmail.com>
2025-02-03 20:43:08 +0100

    refactor: add outside click function

--- social-app/src/features/profile/components/FollowerModal.jsx
+++ social-app/src/features/profile/components/FollowerModal.jsx
@@ -1,16 +1,26 @@
 import PropTypes from "prop-types";
 import ListUsers from "./ListUsers";
+import { useOutsideClick } from "../../../hooks/useOutsideClick";
 
 export function FollowerModal({ setShowFollowers, setShowFollowing, showFollowers, showFollowing }) {
+  const close = () => {
+    if (showFollowers) {
+      setShowFollowers(false);
+    } else if (showFollowing) {
+      setShowFollowing(false);
+    }
+  };
+
+  const ref = useOutsideClick(close);
   return (
     <div className="fixed inset-0 z-50 bg-gray-900 bg-opacity-50 flex justify-center items-center">
-      <div className="w-[90%] max-w-[500px] rounded-xl bg-gray-800 blur-none filter-none transition-all duration-300 lg:w-[70%]">
+      <div
+        className="w-[90%] max-w-[500px] rounded-xl bg-gray-800 blur-none filter-none transition-all duration-300 lg:w-[70%]"
+        ref={ref}
+      >
         <div className="relative m-5 flex h-20 items-center justify-center bg-gray-800">
           <h2 className="text-lg font-semibold text-white">{showFollowers ? "Followers" : "Following"}</h2>
-          <button
-            className="absolute right-0 top-0 m-4 text-5xl text-cyan-50"
-            onClick={() => (showFollowers ? setShowFollowers(false) : setShowFollowing(false))}
-          >
+          <button className="absolute right-0 top-0 m-4 text-5xl text-cyan-50" onClick={close}>
             &times;
           </button>
         </div>
