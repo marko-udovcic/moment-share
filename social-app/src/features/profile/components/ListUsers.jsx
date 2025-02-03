828b3345730ef5d46ea7b6385fbf470cf018a54d
marko udovcic <markoudovcic53@gmail.com>
2025-02-03 20:45:17 +0100

    refactor: use custom hook and zustand profile store for get current user,followers,following

--- social-app/src/features/profile/components/ListUsers.jsx
+++ social-app/src/features/profile/components/ListUsers.jsx
@@ -1,10 +1,17 @@
 import PropTypes from "prop-types";
 import User from "./User";
 import { useProfile } from "../../../context/ProfileContext";
+import { useFollowers } from "../hooks/useFollowers";
+import { useProfileStore } from "../../../context/zustand/useProfileStore";
+import { useFollowing } from "../hooks/useFollowing";
 export default function ListUsers({ showFollowers, showFollowing }) {
-  const { listFollowing, listFollowers } = useProfile();
+  const { listFollowing } = useProfile();
+  const { currentUser } = useProfileStore();
+  const { myFollowers } = useFollowers(currentUser?.id);
+  const { myFollowing } = useFollowing(currentUser?.id);
+  console.log("myFollowing", myFollowing);
+  const showList = showFollowers ? myFollowers : myFollowing;
 
-  const showList = showFollowers ? listFollowers : listFollowing;
   return (
     <ul className="max-h-[20rem] overflow-y-auto p-3">
       {showList.map((follower) => (
