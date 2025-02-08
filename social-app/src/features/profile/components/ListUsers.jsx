5ff12ad99b2cf522efc9bc7336f6cb2da59f912a
marko udovcic <markoudovcic53@gmail.com>
2025-02-08 20:29:14 +0100

    refactor: add logic for remove follower/following

--- social-app/src/features/profile/components/ListUsers.jsx
+++ social-app/src/features/profile/components/ListUsers.jsx
@@ -1,17 +1,31 @@
 import PropTypes from "prop-types";
 import User from "./User";
-import { useProfile } from "../../../context/ProfileContext";
 import { useFollowers } from "../hooks/useFollowers";
 import { useProfileStore } from "../../../context/zustand/useProfileStore";
 import { useFollowing } from "../hooks/useFollowing";
+import { useDeleteFollowing } from "../hooks/useDeleteFollowing";
+import { useDeleteFollower } from "../hooks/useDeleteFollower";
 export default function ListUsers({ showFollowers, showFollowing }) {
-  const { listFollowing } = useProfile();
   const { currentUser } = useProfileStore();
   const { myFollowers } = useFollowers(currentUser?.id);
-  const { myFollowing } = useFollowing(currentUser?.id);
-  console.log("myFollowing", myFollowing);
+  const { myFollowing, isLoading } = useFollowing(currentUser?.id);
+  const { removeFollowing } = useDeleteFollowing();
+  const { removeFollower } = useDeleteFollower();
+
+  if (isLoading || !myFollowers) {
+    return <p>Loading...</p>;
+  }
   const showList = showFollowers ? myFollowers : myFollowing;
 
+  function removeClickedFollower(userId) {
+    const confirm = window.confirm("are you sure you want remove user");
+    if (!confirm) return;
+    if (showFollowers) {
+      removeFollower({ currentUserId: currentUser?.id, followerId: userId });
+    } else {
+      removeFollowing({ currentUserId: currentUser?.id, followingId: userId });
+    }
+  }
   return (
     <ul className="max-h-[20rem] overflow-y-auto p-3">
       {showList.map((follower) => (
@@ -20,6 +34,7 @@ export default function ListUsers({ showFollowers, showFollowing }) {
           key={follower.id}
           showFollowers={showFollowers}
           showFollowing={showFollowing}
+          onClick={removeClickedFollower}
         />
       ))}
     </ul>
