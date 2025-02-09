3fde3c6a9c13ddc04c14e644ac10cc277591c615
marko udovcic <markoudovcic53@gmail.com>
2025-02-09 20:27:37 +0100

    refactor: add boolean for check is user authenticated

--- social-app/src/features/profile/hooks/useCurrentUser.js
+++ social-app/src/features/profile/hooks/useCurrentUser.js
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCurrentUser } from "../../auth/services/apiAuth";
 import { useProfileStore } from "../../../context/zustand/useProfileStore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function useCurrentUser() {
   const setCurrentUser = useProfileStore((state) => state.setCurrentUser);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const { data: currentUser, isLoading } = useQuery({
     queryKey: ["currentUser"],
     queryFn: getCurrentUser,
@@ -16,8 +17,9 @@ export function useCurrentUser() {
   useEffect(() => {
     if (currentUser) {
       setCurrentUser(currentUser);
+      setIsAuthenticated(true);
     }
   }, [currentUser, setCurrentUser]);
 
-  return { user: currentUser, isLoading };
+  return { user: currentUser, isLoading, isAuthenticated };
 }
