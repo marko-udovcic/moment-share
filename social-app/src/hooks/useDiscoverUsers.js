b7ef0b455bdbe6e3372827703e5a2edc6a3fa98f
marko udovcic <markoudovcic53@gmail.com>
2025-02-10 21:31:00 +0100

    refactor: add custom hooks for get followers/following

--- social-app/src/hooks/useDiscoverUsers.js
+++ social-app/src/hooks/useDiscoverUsers.js
@@ -1,14 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 import { getDiscoverUsers as getDiscoverUsersApi } from "../services/apiUsers";
+import { useFollowing } from "../features/profile/hooks/useFollowing";
+import { useFollowers } from "../features/profile/hooks/useFollowers";
 
-export function useDiscoverUsers(currentUserId, myFollowing, myFollowers) {
+export function useDiscoverUsers(currentUserId) {
+  const { myFollowing, isLoading: isLoadingFollowing } = useFollowing(currentUserId);
+  const { myFollowers, isLoading: isLoadingFollowers } = useFollowers(currentUserId);
+
   let followList = [];
-  followList = [
-    ...followList,
-    currentUserId,
-    ...myFollowing.map((user) => String(user.id)),
-    ...myFollowers.map((user) => String(user.id)),
-  ];
+  if (myFollowing && myFollowers) {
+    followList = [
+      currentUserId,
+      ...myFollowing.map((user) => String(user.id)),
+      ...myFollowers.map((user) => String(user.id)),
+    ];
+  }
+
   const {
     data: discoverUsers,
     error,
@@ -16,10 +23,15 @@ export function useDiscoverUsers(currentUserId, myFollowing, myFollowers) {
   } = useQuery({
     queryKey: ["discoverUsers", ...followList],
     queryFn: () => getDiscoverUsersApi(followList),
+    enabled: followList.length > 0,
   });
+
   if (error) {
     console.error("Error fetching discover users:", error);
   }
+  if (isLoadingFollowing || isLoadingFollowers) {
+    return { discoverUsers: [], isLoading: true };
+  }
 
-  return { discoverUsers, isLoading };
+  return { discoverUsers: discoverUsers || [], isLoading: isLoading };
 }
