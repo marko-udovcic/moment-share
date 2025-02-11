dc8a88559a55c08f1f6cef84a72c5098aaed6003
marko udovcic <markoudovcic53@gmail.com>
2025-02-11 19:21:25 +0100

    feat: create profileUser object and passed as a props in Header

--- social-app/src/pages/UserProfile.jsx
+++ social-app/src/pages/UserProfile.jsx
@@ -1,8 +1,20 @@
 import { useParams } from "react-router-dom";
-
+import Nav from "../components/Nav";
+import Header from "../components/Header";
 function UserProfile() {
-  const { id } = useParams();
-  return <div className="text-white">UserProfile id : {id}</div>;
+  const { id, username } = useParams();
+  const profileUser = {
+    id,
+    username,
+  };
+
+  return (
+    <>
+      <Nav />
+      <Header profileUser={profileUser} />
+      <h1 className="text-white">{username}</h1>
+    </>
+  );
 }
 
 export default UserProfile;
