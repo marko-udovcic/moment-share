c80a186d642a116e48b0dd716788f970d813a0b4
marko udovcic <markoudovcic53@gmail.com>
2025-02-12 21:31:44 +0100

    refactor: add useEffect(remove clicked user from global variable) and add custom hooks

--- social-app/src/pages/UserProfile.jsx
+++ social-app/src/pages/UserProfile.jsx
@@ -1,18 +1,23 @@
-import { useParams } from "react-router-dom";
 import Nav from "../components/Nav";
 import Header from "../components/Header";
+import { useProfileUser } from "../features/explore/hooks/useProfileUser";
+import { useProfileStore } from "../context/zustand/useProfileStore";
+import { useEffect } from "react";
+import ListCards from "../components/ListCards";
 function UserProfile() {
-  const { id, username } = useParams();
-  const profileUser = {
-    id,
-    username,
-  };
+  const profileUser = useProfileUser();
+  const { resetProfile } = useProfileStore();
 
+  useEffect(() => {
+    return () => {
+      resetProfile();
+    };
+  }, [resetProfile]);
   return (
     <>
       <Nav />
       <Header profileUser={profileUser} />
-      <h1 className="text-white">{username}</h1>
+      <ListCards />
     </>
   );
 }
