3d651bbb55424bfbbafda3174ab48798ee7bb8d6
marko udovcic <markoudovcic53@gmail.com>
2025-02-12 21:33:50 +0100

    refactor: add userProfile(clicked profile) and fetching data depending on the active user

--- social-app/src/features/profile/hooks/useFetchMoments.js
+++ social-app/src/features/profile/hooks/useFetchMoments.js
@@ -2,13 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import { getUserMoments as getUserMomentsApi } from "../../../services/apiMoments";
 import { useProfileStore } from "../../../context/zustand/useProfileStore";
 export function useFetchMoments() {
-  const { currentUser } = useProfileStore();
+  const { currentUser, userProfile } = useProfileStore();
 
+  const activeUser = userProfile ? userProfile : currentUser;
   const { data: myMoments, error } = useQuery({
-    queryKey: ["myMoments", currentUser?.id],
-    queryFn: () => getUserMomentsApi(currentUser?.id),
+    queryKey: ["myMoments", activeUser?.id],
+    queryFn: () => getUserMomentsApi(activeUser?.id),
     staleTime: 1000 * 60 * 1,
-    enabled: !!currentUser?.id,
+    enabled: !!activeUser?.id,
   });
 
   if (error) {
