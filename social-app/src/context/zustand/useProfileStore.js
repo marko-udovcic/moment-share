ac8e93594b1c50e4efff41b628d34c3daf3b4f13
marko udovcic <markoudovcic53@gmail.com>
2025-02-14 21:52:56 +0100

    refactor: update resetProfile and add global clicked profile

--- social-app/src/context/zustand/useProfileStore.js
+++ social-app/src/context/zustand/useProfileStore.js
@@ -1,8 +1,10 @@
 import { create } from "zustand";
 export const useProfileStore = create((set) => ({
   currentUser: null,
+  userProfile: null,
   isUserLoading: false,
   setCurrentUser: (user) => set({ currentUser: user }),
+  setUserProfile: (user) => set({ userProfile: user }),
   setIsUserLoading: (isLoading) => set({ isUserLoading: isLoading }),
-  resetProfile: () => set({ currentUser: null, isUserLoading: false }),
+  resetProfile: () => set({ userProfile: null }),
 }));
