f7f7d8d31b0d36f6f4b3669870e162b7ffc9acb1
marko udovcic <markoudovcic53@gmail.com>
2025-02-15 15:30:17 +0100

    remove fetch functions and add custom hook for get list of user moments

--- social-app/src/context/ProfileContext.jsx
+++ social-app/src/context/ProfileContext.jsx
@@ -1,60 +1,15 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import PropTypes from "prop-types";
-import { useCallback } from "react";
+import { useFetchMoments } from "../features/profile/hooks/useFetchMoments";
 const ProfileContext = createContext();
 
 function ProfileProvider({ children }) {
-  const [listFollowers, setListFollowers] = useState([]);
-  const [listFollowing, setListFollowing] = useState([]);
-  const [listMoments, setListMoments] = useState([]);
-
-  const fetchData = useCallback(async (url, setList) => {
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      setList(data);
-    } catch (error) {
-      console.error("Error: ", error);
-    }
-  }, []);
-
-  const fetchUsers = useCallback(() => {
-    Promise.all([
-      fetch("/followme")
-        .then((response) => response.json())
-        .then((data) => setListFollowers(data)),
-      fetch("/following")
-        .then((response) => response.json())
-        .then((data) => setListFollowing(data)),
-    ]).catch(() => {
-      console.log("Error during loading");
-    });
-  }, []);
-  const refreshData = useCallback(async () => {
-    await fetchData("/posts", setListMoments);
-    await fetchUsers();
-  }, [fetchData, fetchUsers]);
-
-  useEffect(() => {
-    async function fetchInitialData() {
-      await fetchData("/posts", setListMoments);
-      await fetchUsers();
-    }
-    fetchInitialData();
-  }, [fetchData, fetchUsers]);
+  const myMoments = useFetchMoments();
 
   return (
     <ProfileContext.Provider
       value={{
-        listFollowers,
-        setListFollowers,
-        listFollowing,
-        setListFollowing,
-        listMoments,
-        setListMoments,
-        fetchData,
-        fetchUsers,
-        refreshData,
+        myMoments,
       }}
     >
       {children}
@@ -62,15 +17,11 @@ function ProfileProvider({ children }) {
   );
 }
 
-function useProfile() {
-  const context = useContext(ProfileContext);
-  if (context === undefined) {
-    throw new Error("useProfile must be used within a ProfileProvider");
-  }
-  return context;
+export function useProfile() {
+  return useContext(ProfileContext);
 }
 
-export { ProfileProvider, useProfile };
+export { ProfileProvider };
 ProfileProvider.propTypes = {
   children: PropTypes.any,
 };
