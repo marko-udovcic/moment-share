5738327547063b9f35fa1ae3db48590be603c12b
marko udovcic <markoudovcic53@gmail.com>
2025-02-18 21:29:56 +0100

    feat: add truncateContent(display only 30 words on card)

--- social-app/src/components/ListCards.jsx
+++ social-app/src/components/ListCards.jsx
@@ -1,4 +1,5 @@
 import { RxDotsVertical } from "react-icons/rx";
+import { NavLink } from "react-router-dom";
 import Card from "./ui/Card";
 import Button from "./ui/Button";
 import { getLuminance } from "../utils/colorUtils";
@@ -7,6 +8,7 @@ import { useDeleteMoment } from "../features/profile/hooks/useDeleteMoment";
 import moment from "moment";
 import { useProfile } from "../context/ProfileContext";
 import { useProfileStore } from "../context/zustand/useProfileStore";
+import { truncateContent } from "../utils/truncateContent";
 
 export default function ListCards() {
   let { myMoments } = useProfile();
@@ -14,7 +16,8 @@ export default function ListCards() {
   myMoments = myMoments?.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
   const { deleteMoment, isLoading } = useDeleteMoment();
 
-  async function handleRemoveMoment(id) {
+  async function handleRemoveMoment(e, id) {
+    e.preventDefault();
     const confirm = window.confirm("are you sure you want remove moment");
     if (!confirm) return;
     deleteMoment(id);
@@ -29,38 +32,39 @@ export default function ListCards() {
         myMoments.map((myMoment) => {
           const textColor = getLuminance(myMoment.color) > 128 ? "text-black" : "text-white";
           const date = moment(myMoment.created_at).calendar();
+          const displayContent = truncateContent(myMoment.content, 20, 30);
 
           return (
-            <Card
-              key={myMoment.id}
-              className={
-                "relative flex h-[20rem] flex-col justify-between overflow-hidden rounded-[4px] p-3 text-center "
-              }
-              style={{ backgroundColor: myMoment.color }}
-            >
-              {!userProfile && (
-                <Button
-                  disabled={isLoading}
-                  onClick={() => handleRemoveMoment(myMoment.id)}
-                  className={
-                    "mt-3 self-end p-1 transition duration-300 font-semibold text-white bg-[#000000] bg-opacity-30 rounded-full"
-                  }
-                >
-                  <RxDotsVertical className="text-[1.2rem]" />
-                </Button>
-              )}
-              <div
-                className="relative flex flex-grow items-center justify-center overflow-auto rounded-[20px]"
+            <NavLink key={myMoment.id} to={`/moment/${myMoment.id}`}>
+              <Card
+                key={myMoment.id}
+                className={
+                  "relative flex h-[20rem] flex-col justify-between overflow-hidden rounded-[4px] p-3 text-center "
+                }
                 style={{ backgroundColor: myMoment.color }}
               >
-                <Reveal>
-                  <div className="max-h-full overflow-y-auto text-center">
-                    <h3 className={`break-words text-lg font-medium ${textColor}`}>{myMoment.content}</h3>
-                  </div>
-                </Reveal>
-              </div>
-              <p className={`absolute bottom-0 left-0 m-4 text-sm ${textColor}`}>{date}</p>
-            </Card>
+                {!userProfile && (
+                  <Button
+                    disabled={isLoading}
+                    onClick={(e) => handleRemoveMoment(e, myMoment.id)}
+                    className={
+                      "mt-3 self-end p-1 transition duration-300 font-semibold text-white bg-[#000000] bg-opacity-30 rounded-full"
+                    }
+                  >
+                    <RxDotsVertical className="text-[1.2rem]" />
+                  </Button>
+                )}
+                <div className="relative flex flex-grow w-full items-center justify-center rounded-[20px] mb-6 p-3 text-center">
+                  <Reveal>
+                    <h3 className={`break-words text-lg font-medium leading-normal ${textColor}`}>
+                      {displayContent}
+                    </h3>
+                  </Reveal>
+                </div>
+
+                <p className={`absolute bottom-0 left-0 m-4 text-sm ${textColor}`}>{date}</p>
+              </Card>
+            </NavLink>
           );
         })
       ) : (
