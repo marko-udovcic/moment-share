085068856b8ef6e496355cd33ac4cdfca61d3d80
marko udovcic <markoudovcic53@gmail.com>
2025-02-18 21:30:52 +0100

    refactor: add nav link when click on user username

--- social-app/src/features/profile/components/User.jsx
+++ social-app/src/features/profile/components/User.jsx
@@ -1,11 +1,14 @@
 import Button from "../../../components/ui/Button";
 import PropTypes from "prop-types";
-
+import { NavLink } from "react-router-dom";
 export default function User({ follower, showFollowers, onClick }) {
   return (
     <>
       <li className="m-4 flex items-center justify-between p-3">
-        <h2 className="text-lg font-semibold text-white">{follower.username}</h2>
+        <NavLink key={follower.id} to={`/user/${follower.id}/${follower.username}`}>
+          <h2 className="text-lg font-semibold text-white underline">{follower.username}</h2>
+        </NavLink>
+
         <Button
           onClick={() => onClick(follower.id)}
           className="rounded-lg bg-gray-500 p-2 text-white hover:bg-gray-400"
