efeb29463befc66f7f1bc213a4623c0cb1c3d069
marko udovcic <markoudovcic53@gmail.com>
2025-02-20 18:18:00 +0100

    add button variants

--- social-app/src/components/ui/Button.jsx
+++ social-app/src/components/ui/Button.jsx
@@ -1,8 +1,12 @@
 import PropTypes from "prop-types";
 
-export default function Button({ children, onClick, className, disabled }) {
+const variants = {
+  secondary: `text-gray-700 bg-gray-100 rounded-lg hover:bg-gray-200 active:bg-gray-300 transition 
+    duration-200 ease-in-out dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-gray-600`,
+};
+export default function Button({ children, onClick, className, disabled, variant }) {
   return (
-    <button onClick={onClick} className={`${className}`} disabled={disabled}>
+    <button onClick={onClick} className={`${className} ${variants[variant]}`} disabled={disabled}>
       {children}
     </button>
   );
@@ -13,4 +17,5 @@ Button.propTypes = {
   onClick: PropTypes.func,
   className: PropTypes.string,
   disabled: PropTypes.bool,
+  variant: PropTypes.string,
 };
