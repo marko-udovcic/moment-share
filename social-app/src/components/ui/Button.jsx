e285af97b591eb088504a6dd2555f38fe7c9dc25
marko udovcic <markoudovcic53@gmail.com>
2025-02-23 22:40:50 +0100

    add new variant

--- social-app/src/components/ui/Button.jsx
+++ social-app/src/components/ui/Button.jsx
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 const variants = {
   secondary: `text-gray-700 bg-gray-100 rounded-lg hover:bg-gray-200 active:bg-gray-300 transition 
     duration-200 ease-in-out dark:bg-gray-700 dark:text-gray-200 dark:hover:bg-gray-600`,
+  danger: `text-red-500 p-2 mt-4 rounded lg:ml-4 transition duration-500 ease-in-out lg:hover:bg-red-700
+            lg:hover:text-white`,
 };
 export default function Button({ children, onClick, className, disabled, variant }) {
   return (
