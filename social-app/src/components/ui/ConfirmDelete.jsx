0a498ba94352d196fa566a7e1277723e323f8133
marko udovcic <markoudovcic53@gmail.com>
2025-02-23 22:43:08 +0100

    feat: add confirm delete component

---
+++ social-app/src/components/ui/ConfirmDelete.jsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+function ConfirmDelete({ onCloseModal, onConfirm, name }) {
+  const handleDelete = () => {
+    console.log("Delete button clicked");
+    onConfirm(); // Pozovi funkciju za brisanje
+    onCloseModal(); // Zatvori modal
+  };
+
+  return (
+    <div>
+      <h2 className="text-xl font-semibold text-white">Are you sure that you want delete this {name}?</h2>
+      <div className="flex flex-col lg:flex-row-reverse">
+        <Button variant="danger" onClick={handleDelete}>
+          Delete
+        </Button>
+        <Button className="text-white mt-4" onClick={onCloseModal}>
+          Cancel
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+ConfirmDelete.propTypes = {
+  onCloseModal: PropTypes.func,
+  onConfirm: PropTypes.func,
+  name: PropTypes.string,
+};
+
+export default ConfirmDelete;
