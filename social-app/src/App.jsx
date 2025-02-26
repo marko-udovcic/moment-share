7979712c9aaf18bf897d0344ef00ac3cf6998fa4
marko udovcic <markoudovcic53@gmail.com>
2025-02-26 11:10:16 +0100

    refactor : add lazy loading and fallback

--- social-app/src/App.jsx
+++ social-app/src/App.jsx
@@ -1,37 +1,40 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ProfileProvider } from "./context/ProfileContext";
-import Create from "./pages/Create";
-import Profile from "./pages/Profile";
-import Home from "./pages/Home";
-import Explore from "./pages/Explore";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import { Toaster } from "react-hot-toast";
+import { Suspense, lazy } from "react";
 import ProtectedRoute from "./components/ui/ProtectedRoute";
-import UserProfile from "./pages/UserProfile";
-import Moment from "./pages/Moment";
-
+const Create = lazy(() => import("./pages/Create"));
+const Profile = lazy(() => import("./pages/Profile"));
+const Home = lazy(() => import("./pages/Home"));
+const Explore = lazy(() => import("./pages/Explore"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const UserProfile = lazy(() => import("./pages/UserProfile"));
+const Moment = lazy(() => import("./pages/Moment"));
 export default function App() {
   return (
-    <ProfileProvider>
-      <Router>
-        <div
-          className="sm:max-w-[calc(100%-160px)] mx-auto min-h-screen border-[2px] border-t-0 border-white bg-slate-900 px-0 pt-4
-            relative lg:mx-[5%] 2xl:mx-[10%]"
-        >
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route element={<ProtectedRoute />}>
-              <Route path="/" element={<Home />} />
-              <Route path="/explore" element={<Explore />} />
-              <Route path="/create" element={<Create />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/user/:id/:username" element={<UserProfile />} />
-              <Route path="/moment/:id" element={<Moment />} />
-            </Route>
-          </Routes>
-        </div>
-      </Router>
-    </ProfileProvider>
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <ProfileProvider>
+        <Router>
+          <Suspense fallback={<div>Loading...</div>}>
+            <div className="sm:max-w-[calc(100%-160px)] mx-auto min-h-screen bg-slate-900 px-0 pt-4 relative lg:mx-[5%] 2xl:mx-[10%]">
+              <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route element={<ProtectedRoute />}>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/explore" element={<Explore />} />
+                  <Route path="/create" element={<Create />} />
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="/user/:id/:username" element={<UserProfile />} />
+                  <Route path="/moment/:id" element={<Moment />} />
+                </Route>
+              </Routes>
+            </div>
+          </Suspense>
+        </Router>
+      </ProfileProvider>
+    </>
   );
 }
